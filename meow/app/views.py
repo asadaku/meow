59de8209ac1eb5c1a43b2a8c641e80a219e75f79
Asad Ali <aaak84@gmail.com>
2020-09-12 18:00:28 -0700

    Update to views
    1. New POST view: post_a_cat. Used for saving images.
    2. Updated GET view: get_a_cat. Used for pulling images from google bucket.

--- meow/app/views.py
+++ meow/app/views.py
@@ -1,9 +1,15 @@
+import io
+import random
+import uuid
+
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
-from django.http import HttpResponse
-import numpy as np
+from google.cloud import storage
 from PIL import Image
-import io
+
+from meow.settings import GCLOUD_UNVERIFIED_BUCKET_NAME
+
 
 def index(request):
     return render(request, 'app/index.html')
@@ -11,8 +17,26 @@ def index(request):
 
 @require_http_methods(["GET"])
 def get_a_cat(request):
-    raw = np.random.randn(400,400,3)
-    img = Image.fromarray(raw.astype(np.uint8)).convert('RGB')
+    # For now, pulls a random image from the unverified bucket and displays it
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(GCLOUD_UNVERIFIED_BUCKET_NAME)
+    return JsonResponse({"image_url": random.choice(list(bucket.list_blobs())).public_url})
+
+
+@require_http_methods(["POST"])
+def post_a_cat(request):
+    # Convert bytes to image
+    img = Image.open(request.FILES['cat_image'].file)
+
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(GCLOUD_UNVERIFIED_BUCKET_NAME)
+    filename = "kitty_" + str(uuid.uuid1()) + ".JPEG"
+    blob = bucket.blob(filename)
+
     with io.BytesIO() as output:
+        # Assumes images fit in memory
         img.save(output, format="JPEG")
-        return HttpResponse(output.getvalue(), content_type="image/jpeg")
+        output.seek(0)
+        blob.upload_from_file(output)
+
+    return JsonResponse({"status": "ok"})
